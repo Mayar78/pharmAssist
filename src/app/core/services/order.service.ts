db1b926514c943a726fa82db8fccc067dc737062
Asmaa-A7med <asmaaahmed02758@gmail.com>
2025-06-17 00:17:14 +0300

    order status

--- src/app/core/services/order.service.ts
+++ src/app/core/services/order.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { enviroments } from '../enviroments/enviroment';
 import { Observable } from 'rxjs/internal/Observable';
+import { Order } from '../interfaces/order';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,27 @@ export class OrderService {
 
   constructor(private http: HttpClient) {}
 
-  getUserOrders(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/api/RequestOrder`);
-  }
+  // getUserOrders(): Observable<any> {
+  //   return this.http.get(`${this.baseUrl}/api/RequestOrder`);
+  // }
+
+getUserOrders(): Observable<Order[]> {
+  const token = sessionStorage.getItem('token');
+  const headers = {
+    Authorization: `Bearer ${token}`
+  };
+
+  return this.http.get<Order[]>(`${this.baseUrl}/api/orders`, { headers });
+}
+
+
+getOrderById(id: number): Observable<Order> {
+  const token = sessionStorage.getItem('token');
+  const headers = {
+    Authorization: `Bearer ${token}`
+  };
+
+  return this.http.get<Order>(`${this.baseUrl}/api/orders/${id}`, { headers });
+}
+
 }
